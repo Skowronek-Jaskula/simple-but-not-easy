04351edccd5adbb11d3b2b8b80ee1b602a761538
Anna Skowronek-Jaskula <anna.skowronek-jaskula@capgemini.com>
2022-12-08 15:44:36 +0100

    SIMPLE: grid approach

--- stylelint.config.js
+++ stylelint.config.js
@@ -7,5 +7,15 @@ module.exports = {
     "rule-empty-line-before": "always",
     "declaration-no-important": true,
     "order/properties-alphabetical-order": true,
+    "selector-class-pattern":
+      "([a-z]+)*(?:__[a-z]+(?:-[a-z]+)*)?(?:--[a-z]+(?:-[a-z]+)*)?(?:\\[.+\\])?",
+    // "scss/percent-placeholder-pattern":
+    //   "([a-z]+)*(?:__[a-z]+(?:-[a-z]+)*)?(?:--[a-z]+(?:-[a-z]+)*)?(?:\\[.+\\])?",
+    "selector-no-qualifying-type": [
+      true,
+      {
+        ignore: ["attribute", "class"],
+      },
+    ],
   },
 };
